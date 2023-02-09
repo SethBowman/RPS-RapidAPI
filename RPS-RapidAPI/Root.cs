8ffa413eb1e42c95e528aff2aee29a5ca80d4928
Seth Bowman <brandonsethbowman@gmail.com>
2023-02-09 15:22:38 -0600

    Fixed issue with result

--- RPS-RapidAPI/Root.cs
+++ RPS-RapidAPI/Root.cs
@@ -6,27 +6,29 @@ public class Root
 {
     public static string Game()
     {
+        Console.WriteLine("Rock, Paper, or Scissors?");
+
         var key = File.ReadAllText("appsettings.json");
         var apiKey = JObject.Parse(key).GetValue("DefaultKey").ToString();
 
-        var client = new RestClient("https://rock-paper-scissors9.p.rapidapi.com/?choice=rock");
+        var answer = Console.ReadLine().ToLower();
+
+        var client = new RestClient($"https://rock-paper-scissors9.p.rapidapi.com/?choice={answer}");
         var request = new RestRequest();
         request.AddHeader("X-RapidAPI-Key", $"{apiKey}");
         request.AddHeader("X-RapidAPI-Host", "rock-paper-scissors9.p.rapidapi.com");
         var response = client.Execute(request);
         var root = JsonConvert.DeserializeObject<Root>(response.Content);
 
-        Console.WriteLine("Rock, Paper, or Scissors?");
 
-        root.User.Name = Console.ReadLine().ToLower();
 
-        while(root.User.Name != "rock" && root.User.Name != "paper" && root.User.Name != "scissors")
+        while (answer != "rock" && answer != "paper" && answer != "scissors")
         {
             Console.WriteLine("That was not an option, try again.");
-            root.User.Name = Console.ReadLine().ToLower();
+            answer = Console.ReadLine().ToLower();
         }
 
-        return $"Player 1: {root.User.Name}, Player 2 : {root.Ai.Name}\nResult of game: {root.Result}";
+        return $"Player 1: {answer}, Player 2 : {root.Ai.Name}\nResult of game: {root.Result}";
     }
     [JsonProperty("user")]
     public User User { get; set; }
